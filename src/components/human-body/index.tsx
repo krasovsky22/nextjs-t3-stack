182806b38214ece14787d14be8202cd4b338eee9
Vlad Krasovsky <krasovsky@kentik.com>
2023-03-20 15:54:11 -0700

    added tooltip

--- src/components/human-body/index.tsx
+++ src/components/human-body/index.tsx
@@ -1,15 +1,31 @@
-import { Box } from "@chakra-ui/react";
-import { useEffect, useRef } from "react";
+import { Box, Text, Tooltip } from "@chakra-ui/react";
+import { debounce } from "lodash";
+import { useEffect, useRef, useState } from "react";
 
 import FrontBody from "./front";
 
 const HumanBody = () => {
   const ref = useRef<HTMLDivElement | null>(null);
+  const textRef = useRef<HTMLDivElement | null>(null);
+  const [tooltip, setTooltip] = useState<string | null>(null);
 
   useEffect(() => {
+    const element = ref.current as HTMLDivElement;
+
     const onMouseOver = (event: MouseEvent) => {
       const target = event.target as SVGElement;
+      const tooltip = textRef.current;
       if (target.parentElement?.id) {
+        setTooltip(target.parentElement?.id);
+        // if (tooltip) {
+        //   const title = target.parentElement?.id;
+
+        //   tooltip.innerHTML = title;
+        //   tooltip.style.display = "block";
+        //   tooltip.style.left = `${event.offsetX - 20}px`;
+        //   tooltip.style.top = `${event.offsetY - 20}px`;
+        // }
+
         target.parentElement.querySelectorAll("path").forEach((path) => {
           path.style.fill = "red";
         });
@@ -22,21 +38,32 @@ const HumanBody = () => {
         target.parentElement.querySelectorAll("path").forEach((path) => {
           path.style.fill = "currentColor";
         });
+
+        setTooltip(null);
+
+        // if (textRef.current) {
+        //   textRef.current.style.display = "none";
+        // }
       }
     };
 
-    ref.current?.addEventListener("mouseover", onMouseOver);
-    ref.current?.addEventListener("mouseout", onMouseLeave);
+    console.log("adding event listener");
+    element?.addEventListener("mouseover", onMouseOver);
+    element?.addEventListener("mouseout", onMouseLeave);
 
     return () => {
-      ref.current?.removeEventListener("mouseover", onMouseOver);
-      ref.current?.removeEventListener("mouseout", onMouseLeave);
+      element?.removeEventListener("mouseover", onMouseOver);
+      element?.removeEventListener("mouseout", onMouseLeave);
     };
   }, []);
   return (
-    <Box flex="1" position="relative" h="100%" ref={ref}>
-      <FrontBody />
-    </Box>
+    <Tooltip isOpen label={tooltip} placement="top">
+      <Box flex="1" position="relative" ref={ref} h="80%">
+        <Box h="100%" position="relative">
+          <FrontBody />
+        </Box>
+      </Box>
+    </Tooltip>
   );
 };
 
