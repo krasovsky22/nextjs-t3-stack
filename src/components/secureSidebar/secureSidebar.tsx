6b3b92a6b8214dd409f2d39e346bf7bcf79691d5
Vlad Krasovsky <krasovsky@kentik.com>
2023-04-08 08:03:02 -0700

    remove logo from sidebar

--- src/components/secureSidebar/secureSidebar.tsx
+++ src/components/secureSidebar/secureSidebar.tsx
@@ -35,9 +35,9 @@ const SecureSidebar = () => {
   return (
     <div className="relative min-h-full border-r-2 border-gray-100 bg-gray-100">
       <div className="border-r-1 w-100 absolute block h-full w-full border-gray-100 bg-white">
-        <div className="mx-8 flex h-20 w-full items-center justify-between ">
+        {/* <div className="mx-8 flex h-20 w-full items-center justify-between ">
           <p className="font-[monospace] text-2xl font-bold">Logo</p>
-        </div>
+        </div> */}
         {sidebarRoutes.map((link) => (
           <Link href={link.url} key={link.url}>
             <div className="flex cursor-pointer items-center gap-4 p-4 hover:bg-cyan-400 hover:text-white">
